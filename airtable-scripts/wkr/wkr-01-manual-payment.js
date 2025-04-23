e2ec77df1a48a92e50d86f7fd74b3749a4f5863c
francisaguilar21 <francis.aguilar11@gmail.com>
2025-04-23 23:05:51 +0800

    Add WKR Automation Script 01

---
+++ airtable-scripts/wkr/wkr-01-manual-payment.js
@@ -0,0 +1,34 @@
+const inputConfig = input.config();
+console.log(inputConfig);
+
+const table = base.getTable("Manual Payments");
+let queryResult = await table.selectRecordAsync(inputConfig.recordId, { 
+    fields: ["ID", "Email", "First Name", "Last Name", "Mobile Number", "Membership", "Created Time"]
+});
+
+const payor = {
+    "airtableRecordId": inputConfig.recordId,
+    "manualPaymentTableId": queryResult?.getCellValue("ID"),
+    "email": queryResult?.getCellValue("Email"),
+    "firstName": queryResult?.getCellValue("First Name"),
+    "lastName": queryResult?.getCellValue("Last Name"),
+    "mobileNumber": queryResult?.getCellValue("Mobile Number"),
+    "membership": queryResult?.getCellValue("Membership").name,
+    "createdTime": queryResult?.getCellValue("Created Time")
+};
+
+console.log(payor);
+
+try {
+    // Manual Payments: 02 Send Email Confirmation (make.com)
+    let response = await fetch('<INSERT WEBHOOK URL>', {
+        method: 'POST',
+        body: JSON.stringify(payor),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    console.log(response.status);
+} catch (error) {
+    console.error(error, Date.now());
+}
